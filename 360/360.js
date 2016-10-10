8126b2e7b7f6e3c2c58c7b3c27b08ef302b31dbf
kolosa <limoyan020@163.com>
2016-10-10 23:34:41 +0800

    2.5

--- 360/360.js
+++ 360/360.js
@@ -1,7 +1,10 @@
 window.onload = function () {
     var oImg = document.getElementById("img1");
     var aImg= document.getElementsByTagName("img");
-    var oLastImg=oImg
+    var oLastImg=oImg;
+    var lastX=0;
+    var iSpeed=0;
+    var timer=null
     var x = 0;
     for(var i=1;i<77;i++){
         var oNewImg=document.createElement("img");
@@ -10,6 +13,7 @@ window.onload = function () {
         document.body.appendChild(oNewImg)
     }
     document.onmousedown = function (ev) {
+        clearInterval(timer);
         var oEvent = ev || event;
         var disX = oEvent.clientX - x;
         document.onmousemove = function (ev) {
@@ -22,16 +26,42 @@ window.onload = function () {
                 l= l + -Math.floor(l / 77) * 77;
             }
             if(oLastImg!=aImg[l]){
-                oLastImg.style.display="none"
-                aImg[l].style.display="block"
-                oLastImg=aImg[l]
+                oLastImg.style.display="none";
+                aImg[l].style.display="block";
+                oLastImg=aImg[l];
             }
+            iSpeed=x-lastX;
+            lastX=x;
             return false;
-
         };
         document.onmouseup = function () {
             document.onmousemove = null;
             document.onmouseup = null;
+            timer=setInterval(function(){
+                x+=iSpeed;
+                if(iSpeed>0){
+                    iSpeed--
+                }else{
+                    iSpeed++
+                }
+                if(iSpeed==0){
+                    clearInterval(timer);
+                }
+                var l = parseInt(-x / 10);
+                if (l > 0) {
+                    l=l%77
+                } else {
+                    l= l + -Math.floor(l / 77) * 77;
+                }
+
+                if(oLastImg!=aImg[l]){
+                    oLastImg.style.display="none";
+                    aImg[l].style.display="block";
+                    oLastImg=aImg[l];
+                }
+
+            },30)
+
         };
         return false
     }
