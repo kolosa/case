d0884803c686c1836eb8d47f4a9ad44fe0a96d4f
kolosa <limoyan020@163.com>
2016-10-13 23:14:06 +0800

    0.2

--- 轮播/turn.js
+++ 轮播/turn.js
@@ -1,8 +1,31 @@
 window.onload = function () {
-    var adiv = getClass("aImg");
-    var aLi=getClass("iImg");
+    var aAll =document.getElementById("all").getElementsByTagName("li");
+    var aWra=document.getElementById("wra").getElementsByTagName("li");
+    var oBtnl=document.getElementById("pre").getElementsByTagName("li");
+    var obtnr=document.getElementById("next").getElementsByTagName("li");
+    var aLitli=document.getElementById("nav").getElementsByTagName("li");
+    var iNow=0
+    var i=0;
 
-        };
+    for(i=0;i<aLitli.length;i++){
+        aLitli[i].index=i;
+        aLitli[i].onclick=function(){
+            clearInterval();
+            for(i=0;i<aLitli.length;i++){
+                if(iNow==this.index){
+                    return
+                }
+                aLitli[i].className="iImg";
+                clearInterval( aAll[i].timer);
+                aAll[i].style.filter='alpha(opacity:0)';
+                aAll[i].style.opacity=0;
+            }
+            this.className="iImg border";
+            startMove(aAll[this.index],{opacity:100});
+            iNow=this.index
+        }
+    }
+};
 
 function getClass(name) {
     if (document.getElementsByClassName) {
@@ -17,4 +40,62 @@ function getClass(name) {
         }
         return el;
     }
+}
+function getStyle(obj, attr)
+{
+    if(obj.currentStyle)
+    {
+        return obj.currentStyle[attr];
+    }
+    else
+    {
+        return getComputedStyle(obj, false)[attr];
+    }
+}
+
+function startMove(obj, json, fn)
+{
+    clearInterval(obj.timer);
+    obj.timer=setInterval(function (){
+        var bStop=true;
+        for(var attr in json)
+        {
+            var iCur=0;
+
+            if(attr=='opacity')
+            {
+                iCur=parseInt(parseFloat(getStyle(obj, attr))*100);
+            }
+            else
+            {
+                iCur=parseInt(getStyle(obj, attr));
+            }
+            var iSpeed=(json[attr]-iCur)/8;
+            iSpeed=iSpeed>0?Math.ceil(iSpeed):Math.floor(iSpeed);
+            if(iCur!=json[attr])
+            {
+                bStop=false;
+            }
+
+            if(attr=='opacity')
+            {
+                obj.style.filter='alpha(opacity:'+(iCur+iSpeed)+')';
+                obj.style.opacity=(iCur+iSpeed)/100;
+            }
+            else
+            {
+                obj.style[attr]=iCur+iSpeed+'px';
+            }
+        }
+
+        if(bStop)
+        {
+            clearInterval(obj.timer);
+
+            if(fn)
+            {
+                fn();
+            }
+        }
+    }, 30)
 }
