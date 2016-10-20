599203bf3762076b9f64165edc4e951f4f806e1d
kolosa <limoyan020@163.com>
2016-10-20 23:42:06 +0800

    0.15

--- count down/cd.js
+++ count down/cd.js
@@ -1,3 +1,17 @@
-/**
- * Created by hasee on 2016/10/18.
- */
+window.onload=function(){
+    
+}
+function getClass(name) {
+    if (document.getElementsByClassName) {
+        return document.getElementsByClassName(name)
+    } else {
+        var el = [],
+            _el = document.getElementsByTagName('*');
+        for (var i = 0; i < _el.length; i++) {
+            if (_el[i].className == name) {
+                el[el.length] = _el[i];
+            }
+        }
+        return el;
+    }
+}
