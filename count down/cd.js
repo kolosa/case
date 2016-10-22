2d4dc8beccdc450a39681d214381bafe193ac8ad
kolosa <limoyan020@163.com>
2016-10-22 23:54:08 +0800

    0.16

--- count down/cd.js
+++ count down/cd.js
@@ -1,6 +1,17 @@
 window.onload=function(){
-    
-}
+    var oIn=document.getElementById("in");
+    var oYear=oIn.getElementsByTagName("input")[0];
+    var oMonth=oIn.getElementsByTagName("input")[1];
+    var oDay=oIn.getElementsByTagName("input")[2];
+    var oBtn=document.getElementById("go");
+
+    var oDateEnd=new Date()
+    oDateEnd.setFullYear(parseInt(oYear.value));
+    oDateEnd.setMonth(parseInt(oMonth.value)-1);
+    oDateEnd.setDate(parseInt(oDay.value));
+
+
+};
 function getClass(name) {
     if (document.getElementsByClassName) {
         return document.getElementsByClassName(name)
