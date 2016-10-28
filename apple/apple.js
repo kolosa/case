f9f7ed633ba823861eeacaf657747ddcf6cfe3b7
kolosa <limoyan020@163.com>
2016-10-28 23:43:08 +0800

    0.5

--- apple/apple.js
+++ apple/apple.js
@@ -1,9 +1,86 @@
 window.onload=function(){
     var oLi=document.getElementById("oimg").getElementsByTagName("li");
+    var aBtn=document.getElementById("img-nav").getElementsByTagName("a");
+    var oLiSave=[];
+    var i=0;
+
     for(var i= 0;i<oLi.length;i++){
         oLi[i].style.left=i*130+"px";
     }
+    for(var i= 0;i<oLi.length;i++){
+        oLiSave[i]=oLi[i].offsetLeft;
+    }
+    aBtn[1].onclick=function(){
+        i=1;
+        startMove(oLi[0],{left:-600},MOVE_TYPE.FLEX)
+        var timer=setInterval(
+            function(){
+                startMove(oLi[i++],{left:-600},MOVE_TYPE.FLEX);
+                if(i>=oLi.length/2){
+                    clearInterval(timer);
+                    next();
+                }
+            },50);
+        function next(){
+            timer=setInterval(function(){
+                startMove(oLi[i],{left:oLiSave[i-oLi.length/2]},MOVE_TYPE.FLEX);
+                i++;
+                if(i>=oLi.length){
+                    clearInterval(timer);
+                }
+            },50)
+
+        }
+    }
+
 };
+function getClass(name) {
+    if (document.getElementsByClassName) {
+        return document.getElementsByClassName(name)
+    } else {
+        var el = [],
+            _el = document.getElementsByTagName('*');
+        for (var i = 0; i < _el.length; i++) {
+            if (_el[i].className == name) {
+                el[el.length] = _el[i];
+            }
+        }
+        return el;
+    }
+}
+
+function css(obj, attr, value)
+{
+    if(arguments.length==2)
+        return parseFloat(obj.currentStyle?obj.currentStyle[attr]:document.defaultView.getComputedStyle(obj, false)[attr]);
+    else if(arguments.length==3)
+        switch(attr)
+        {
+            case 'width':
+            case 'height':
+            case 'paddingLeft':
+            case 'paddingTop':
+            case 'paddingRight':
+            case 'paddingBottom':
+                value=Math.max(value,0);
+            case 'left':
+            case 'top':
+            case 'marginLeft':
+            case 'marginTop':
+            case 'marginRight':
+            case 'marginBottom':
+                obj.style[attr]=value+'px';
+                break;
+            case 'opacity':
+                obj.style.filter="alpha(opacity:"+value*100+")";
+                obj.style.opacity=value;
+                break;
+            default:
+                obj.style[attr]=value;
+        }
+
+    return function (attr_in, value_in){css(obj, attr_in, value_in)};
+}
 
 var MOVE_TYPE={
     BUFFER: 1,
@@ -20,10 +97,10 @@ function startMove(obj, oTarget, iType, fnCallBack, fnDuring)
     switch(iType)
     {
         case MOVE_TYPE.BUFFER:
-            fnMove=miaovDoMoveBuffer;
+            fnMove=bufferMove;
             break;
         case MOVE_TYPE.FLEX:
-            fnMove=miaovDoMoveFlex;
+            fnMove=flexMove;
             break;
     }
 
